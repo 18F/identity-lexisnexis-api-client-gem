866c496559e7882722a061eac9396215c7774f38
Peter Karman <peter.karman@gsa.gov>
2016-06-20 13:53:35 -0500

    Hashie::Mash creates recursive hashes, so use method syntax rather than key-as-symbol syntax

--- lib/proofer/vendor/lexisnexis.rb
+++ lib/proofer/vendor/lexisnexis.rb
@@ -11,13 +11,13 @@ module Proofer
           middle_name: ln_applicant.middle_name,
           last_name: ln_applicant.last_name,
           ssn: ln_applicant.ssn,
-          dob: [ln_applicant.dob[:year], ln_applicant.dob[:month], ln_applicant.dob[:day]].join(''),
+          dob: [ln_applicant.dob.year, ln_applicant.dob.month, ln_applicant.dob.day].join(''),
           phone: ln_applicant.phone,
-          address1: ln_applicant.address[:line_1],
-          address2: ln_applicant.address[:line_2],
-          city: ln_applicant.address[:city],
-          state: ln_applicant.address[:state],
-          zipcode: ln_applicant.address[:zip_code]
+          address1: ln_applicant.address.line_1,
+          address2: ln_applicant.address.line_2,
+          city: ln_applicant.address.city,
+          state: ln_applicant.address.state,
+          zipcode: ln_applicant.address.zip_code
         )
       end
 
