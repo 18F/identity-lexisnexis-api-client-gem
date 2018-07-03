b48a45c6f25252021e43cbf11a7bd655f6491c26
Jonathan Hooper <jon9820@gmail.com>
2018-07-03 14:17:05 -0500

    Add integraton specs (#3)
    * Add integraton specs
    
    **Why**: So we can test that we are able to send a valid request and
    handle a response as expected against the actual LexisNexis API
    
    * Expect failed results to not raise exceptions

---
+++ spec/integration/proofers_spec.rb
@@ -0,0 +1,54 @@
+require 'csv'
+
+describe 'LexisNexis Proofers' do
+  before(:all) do
+    Dotenv.overload('.env')
+  end
+
+  after(:all) do
+    Dotenv.overload('.env.test')
+  end
+
+  CSV.parse(Fixtures.lexisnexis_test_data, headers: true).each_with_index do |datum, index|
+    context "test case #{index}" do
+      let(:applicant) do
+        {
+          uuid: SecureRandom.uuid,
+          first_name: datum['first_name'],
+          last_name: datum['last_name'],
+          ssn: datum['ssn'],
+          dob: datum['dob'],
+          phone: datum['phone'],
+        }
+      end
+
+      it 'proofs correctly with InstantVerify' do
+        applicant.delete(:phone)
+
+        result = LexisNexis::InstantVerify::Proofer.new.proof(applicant)
+
+        if datum['result'] == 'pass'
+          expect(result.success?).to eq(true)
+        elsif datum['result'] == 'failed'
+          expect(result.success?).to eq(false)
+          expect(result.exception).to eq(nil)
+        else
+          raise "Unexpected result type: #{result}"
+        end
+      end
+
+      it 'proofs correctly for PhoneFinder' do
+        result = LexisNexis::PhoneFinder::Proofer.new.proof(applicant)
+
+        if datum['result'] == 'pass'
+          expect(result.success?).to eq(true)
+        elsif datum['result'] == 'failed'
+          expect(result.success?).to eq(false)
+          expect(result.exception).to eq(nil)
+        else
+          raise "Unexpected result type: #{datum['result']}"
+        end
+      end
+    end
+  end
+end
