96ba27d94ad18c863d3af2c5c60aa1b57cd4e4ce
Zach Margolis <zachary.margolis@gsa.gov>
2020-04-08 15:24:54 -0700

    DateFormatter specs

--- spec/lib/date_formatter_spec.rb
+++ spec/lib/date_formatter_spec.rb
@@ -1,29 +1,39 @@
 describe LexisNexis::DateFormatter do
-  describe '#formatted_date' do
-    subject { described_class.new(date_string) }
+  subject(:date_formatter) { described_class.new(date_string) }
+
+  describe '#date' do
+    subject(:date) { date_formatter.date }
 
     context 'with a MM/DD/YYYY formatted date' do
       let(:date_string) { '01/02/1993' }
 
-      it 'returns a properly formatted date hash' do
-        expect(subject.formatted_date).to eq(
-          Year: '1993',
-          Month: '1',
-          Day: '2'
-        )
-      end
+      it { is_expected.to eq(Date.new(1993, 1, 2)) }
     end
 
     context 'with a YYYYMMDD formatted date' do
       let(:date_string) { '19930102' }
 
-      it 'returns a properly formatted date hash' do
-        expect(subject.formatted_date).to eq(
-          Year: '1993',
-          Month: '1',
-          Day: '2'
-        )
-      end
+      it { is_expected.to eq(Date.new(1993, 1, 2)) }
+    end
+  end
+
+  describe '#formatted_date' do
+    let(:date_string) { '04/15/2020' }
+
+    it 'is a hash' do
+      expect(date_formatter.formatted_date).to eq(
+        Year: '2020',
+        Month: '4',
+        Day: '15'
+      )
+    end
+  end
+
+  describe '#yyyymmdd' do
+    let(:date_string) { '01/31/2020' }
+
+    it 'is a correctly-formatted string' do
+      expect(date_formatter.yyyymmdd).to eq('20200131')
     end
   end
 end
