5bde8eeef3c8c1d4bff0c2b4e1591f3f3390a243
Steve Urciuoli <steve.urciuoli@gsa.gov>
2021-01-22 23:42:00 -0600

    LG-3776 Log vendor resolution transaction IDs (#19)

--- spec/support/shared_examples/proofer.rb
+++ spec/support/shared_examples/proofer.rb
@@ -1,11 +1,13 @@
 shared_examples 'a proofer' do
   let(:verification_status) { 'passed' }
+  let(:conversation_id) { 'foo' }
   let(:verification_errors) { {} }
   let(:result) { Proofer::Result.new }
 
   before do
     response = instance_double(LexisNexis::Response)
     allow(response).to receive(:verification_status).and_return(verification_status)
+    allow(response).to receive(:conversation_id).and_return(conversation_id)
     allow(response).to receive(:verification_errors).and_return(verification_errors)
 
     allow(verification_request).to receive(:send).and_return(response)
@@ -21,6 +23,7 @@ shared_examples 'a proofer' do
 
         expect(result.success?).to eq(true)
         expect(result.errors).to be_empty
+        expect(result.transaction_id).to eq(conversation_id)
       end
     end
 
@@ -38,6 +41,7 @@ shared_examples 'a proofer' do
           base: ['test error'],
           Discovery: ['another test error']
         )
+        expect(result.transaction_id).to eq(conversation_id)
       end
     end
   end
